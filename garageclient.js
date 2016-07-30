2a382ba9ba17d9af171d84a64bb6fd7d49f5de10
Kyle Kewley <kylek61294@gmail.com>
2016-07-30 10:46:21 -0700

    Added basic functionality for connecting/disconnecting from mqtt

--- garageclient.js
+++ garageclient.js
@@ -1,47 +1,121 @@
 var mqtt    = require('react-native-mqtt');
+var DeviceInfo = require('react-native-device-info');
 
 class GarageClient {
   constructor() {
-    super();
+    this.clientID = DeviceInfo.getUniqueID() || '';
+    this._client = null;
+
+    // Track pairs of subscription/handler functions
+    this.subscriptions = {};
+  }
+
+  /*
+   * This function will take all of the topic keys in this.subscriptions and
+   * make sure the client is subscribed to each of them
+   */
+  _subscribeToAllTopics(client) {
+    for (var key in this.subscriptions) {
+      client.subscribe(key, this.subscriptions[key].qos);
+    }
+  }
+
+  /*
+   * Subscribe the client to a topic with the given qos. The handler function
+   * is called with the client and the message object when a message is recieved
+   * on the topic.
+   */
+  _subscribeToTopic(topic, qos, handler) {
+    this.subscriptions[topic] = {
+      qos: qos,
+      handler: handler
+    };
+
+    // Subscribe right now if we are already connected
+    if (this._client !== null) {
+      this._client.subscribe(topic, qos);
+    }
+  }
+
+  _defaultMessageHandler(client, msg) {
+    console.log('mqtt message default handler:', msg);
+  }
+
+  /*
+   * Called whenever the client is connected. callback(client)
+   */
+  onConnect(callback) {
+    this.connected = callback;
   }
 
 
-  _uriFromHost(host, port) {
-    return 'mqtt://' + host + ':' + port;
+  /*
+   * Called whenever the client is disconnected. callback()
+   */
+  onDisconnect(callback) {
+    this.disconnected = callback;
   }
 
-  connectToServer(host, port) {
-    var uri = this._uriFromHost(host, port);
+  publish(topic, payload, qos, retain, errorHandler) {
+    if (this._client !== null) {
+      client.publish(topic, payload, qos, retain);
+    } else if (errorHandler) {
+      errorHandler("Client not connected");
+    }
+  }
+
+  connectToServer(host, port, errorHandler) {
+
     mqtt.createClient({
-      uri: uri,
-      clientId: 'your_client_id'
-    }).then(function(client) {
+      host: host,
+      port: port,
+      clientId: this.clientID
+    }).then((client) => {
 
-      client.on('closed', function() {
-        console.log('mqtt.event.closed');
-
-      });
-
-      client.on('error', function(msg) {
+      client.on('error', (msg) => {
         console.log('mqtt.event.error', msg);
-
+        if (errorHandler) {
+          errorHandler(msg);
+        }
       });
 
-      client.on('message', function(msg) {
-        console.log('mqtt.event.message', msg);
+      client.on('message', (msg) => {
+        var topic = msg.topic;
+
+        // use the default handler if nothing else specified
+        var handler = this._defaultMessageHandler;
+
+        // set the handler if one has been added in subscriptions
+        if (topic in this.subscriptions) {
+          var h = this.subscriptions[topic].handler;
+          if (h) handler = h;
+        }
+
+        // Call the handler
+        handler(client, msg);
       });
 
-      client.on('connect', function() {
-        console.log('connected');
-        client.subscribe('/data', 0);
-        client.publish('/data', "test", 0, false);
+      client.on('closed', () => {
+        this.client = null;
+
+        // Execute the callback if set
+        if (this.disconnected) this.disconnected();
+      });
+
+      client.on('connect', () => {
+        this.client = client;
+
+        // Execute the callback if set
+        if (this.connected) this.connected(client);
       });
 
       client.connect();
     }).catch(function(err){
-      console.log(err);
+      console.log("error",err);
+      if (errorHandler) {
+        errorHandler(err);
+      }
     });
-
   }
 }
 

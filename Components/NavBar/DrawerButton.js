eaaeb29529339764cf5d41cd59267356b8090ae7
Kyle Kewley <kylek61294@gmail.com>
2016-07-29 19:39:20 -0700

    Updated Drawer button size

--- Components/NavBar/DrawerButton.js
+++ Components/NavBar/DrawerButton.js
@@ -15,7 +15,7 @@ class DrawerButton extends Component {
     return (
       <View style={styles.drawerButton}>
       <TouchableHighlight underlayColor="#00796B" style={styles.drawerButtonTouchable} onPress={this.props.onPress}>
-        <Icon name="menu" size={35} style={styles.iconLight} />
+        <Icon name="menu" size={24} style={styles.iconLight} />
       </TouchableHighlight>
       </View>
       );
